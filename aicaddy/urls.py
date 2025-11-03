2941f9de71a0a0b032025f22bccda58a0f555ca4
gdjones-526 <gijones526@gmail.com>
2025-11-03 17:10:23 -0600

    changes to name

--- aicaddy/urls.py
+++ aicaddy/urls.py
@@ -1,6 +1,6 @@
 """
 Main URL configuration for the aicaddy project.
-This file includes the URLs from the caddy application.
+This file includes the URLs from the dashboard application.
 """
 from django.contrib import admin
 from django.urls import path, include # Make sure 'include' is imported
@@ -8,6 +8,6 @@ from django.urls import path, include # Make sure 'include' is imported
 urlpatterns = [
     path('admin/', admin.site.urls),
     # This line tells Django that for any URL that isn't '/admin/',
-    # it should look for further instructions in the 'caddy.urls' file.
-    path('', include('caddy.urls')), 
+    # it should look for further instructions in the 'dashboard.urls' file.
+    path('', include('dashboard.urls')), 
 ]
