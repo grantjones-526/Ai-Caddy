7f5ffb620d01d437ec55787d4596f6af0f683c35
gdjones-526 <gijones526@gmail.com>
2025-11-03 16:58:46 -0600

    fixed name issue

--- aicaddy/asgi.py
+++ aicaddy/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for aidashboard project.
+ASGI config for aicaddy project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aidashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aicaddy.settings')
 
 application = get_asgi_application()
