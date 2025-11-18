5e9d1814b94942272a555140ffe3d8cc55f0aa17
grantjones-526 <grantjones526@gmail.com>
2025-11-18 17:01:36 -0600

    fix

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Running database migrations..."
+python manage.py migrate --noinput
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
